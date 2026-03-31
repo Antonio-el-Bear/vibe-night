c478791f2b28e7fc362a573a3c74c6c6aeaf77ad
Antonio-el-Bear <antoniozambuko@gmail.com>
2026-03-31 12:26:03 +0200

    refactor: update app parameter retrieval to use custom VITE_ variables

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   logLevel: 'error', // Suppress warnings, only show errors
